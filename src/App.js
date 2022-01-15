68dc8ab342bac6b03971a5b1f9230a309110be6e
Justin Brubaker <justinbrubaker7@gmail.com>
2022-01-14 17:40:15 -0800

    craco and tailwind set up. Hero section complete

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HeroSections from "./Components/HeroSection.js";
+
+function App() {
+  return (
+    <div className="App">
+      <HeroSections />
+    </div>
+  );
+}
+
+export default App;
