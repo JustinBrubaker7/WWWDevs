ba3ece8f0899e5c17c3ad9ef4483aad36345c938
Justin Brubaker <justinbrubaker7@gmail.com>
2022-01-14 18:20:55 -0800

    feature section and call to action and review

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 import HeroSections from "./Components/HeroSection.js";
+import CallToAction from "./Components/CallToAction.js";
+import FeaturesSection from "./Components/FeaturesSection";
+import ReviewsSection from "./Components/ReviewsSection";
+import FAQSection from "./Components/FAQSection";
+import TheProcessSection from "./Components/TheProcessSection";
 
 function App() {
   return (
     <div className="App">
       <HeroSections />
+      <CallToAction />
+      <FeaturesSection />
+      <TheProcessSection />
+      <ReviewsSection />
+      <FAQSection />
     </div>
   );
 }
